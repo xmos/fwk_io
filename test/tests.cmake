c99b4f82f72681451de7f79de3c2bd61403898bf
Jeremiah McCarthy <jeremiahmccarthy@xmos.com>
2022-05-23 16:25:44 -0400

    Add placeholder tests

--- test/tests.cmake
+++ test/tests.cmake
@@ -1,1 +1,8 @@
 ## TODO
+
+## Add HIL tests
+# include(${CMAKE_CURRENT_LIST_DIR}/hil/lib_i2c/lib_i2c.cmake)
+# include(${CMAKE_CURRENT_LIST_DIR}/hil/lib_i2s/lib_i2s.cmake)
+# # include(${CMAKE_CURRENT_LIST_DIR}/hil/lib_qspi_io/lib_qspi_io.cmake)
+# include(${CMAKE_CURRENT_LIST_DIR}/hil/lib_spi/lib_spi.cmake)
+# include(${CMAKE_CURRENT_LIST_DIR}/hil/lib_uart/lib_uart.cmake)

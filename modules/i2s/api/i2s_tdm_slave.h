8fc1f99640b2aad2ff9271826d59782c076e3f80
Jeremiah McCarthy <jeremiahmccarthy@xmos.com>
2023-06-20 14:41:38 -0400

    Initial TDM API for TX 16
    Note full featured library skeleton in place but not feature full yet.
    
    i2s_tdm_slave_tx_16 currently makes assumptions that may change.

---
+++ modules/i2s/api/i2s_tdm_slave.h
@@ -0,0 +1,198 @@
+// Copyright 2023 XMOS LIMITED.
+// This Software is subject to the terms of the XMOS Public Licence: Version 1.
+#pragma once
+
+#include <xs1.h>
+#include <stdint.h>
+#include <stddef.h>
+
+#include <xcore/port.h>
+#include <xcore/clock.h>
+#include <xcore/parallel.h>
+
+#include "i2s.h"
+
+/**
+ * \addtogroup hil_i2s_tdm_core hil_i2s_tdm_core
+ *
+ * The public API for using the HIL I2S TDM core.
+ * @{
+ */
+
+#define I2S_TDM_MAX_POUT_CNT  4
+#define I2S_TDM_MAX_PIN_CNT   4
+
+/* Max channels per frame.  This is overrideable to enable
+ * memory savings in specific applications by allowing
+ * IO buffers to be optimal size, saving up to
+ * word length bytes * (default val - app defined value) bytes.
+ */
+#ifndef I2S_TDM_MAX_CH_PER_FRAME
+#define I2S_TDM_MAX_CH_PER_FRAME 16
+#endif /* I2S_TDM_MAX_CH_PER_FRAME */
+
+/**
+ * Struct to hold an I2S TDM context.
+ *
+ * The members in this struct should not be accessed directly.
+ */
+typedef struct {
+    i2s_callback_group_t *const i2s_cbg;
+    port_t p_dout[I2S_TDM_MAX_POUT_CNT];
+    size_t num_out;
+    port_t p_din[I2S_TDM_MAX_PIN_CNT];
+    size_t num_in;
+    port_t p_fsync;
+    port_t p_bclk;
+    xclock_t bclk;
+    uint32_t tx_offset;
+    uint32_t fsync_len;
+    uint32_t word_len;
+    uint32_t ch_len;
+    uint32_t ch_per_frame;
+    i2s_slave_bclk_polarity_t slave_bclk_polarity;
+    void *app_data;
+} i2s_tdm_ctx_t;
+
+/**@}*/ // END: addtogroup hil_i2s_tdm_core
+
+DECLARE_JOB(i2s_tdm_slave_tx_16, (const i2s_tdm_ctx_t *));
+DECLARE_JOB(i2s_slave_tdm, (const i2s_tdm_ctx_t *));
+
+/**
+ * \addtogroup hil_i2s_tdm_slave_tx16 hil_i2s_tdm_slave_tx16
+ *
+ * The public API for using the HIL I2S TDM TX 16 slave.
+ * @{
+ */
+
+/**
+ * I2S TDM slave context initialization for 16 channel TX only with
+ * 1 output port, 32b word length, 32b channel length, 
+ * and 16 channels per frame.
+ *
+ * This prepares a context for I2S TDM slave on the provided pins.
+ * 
+ * The resulting context can be used with i2s_tdm_slave_tx_16_thread().
+ *
+ * \param ctx            A pointer to the I2S TDM context to use.
+ * \param i2s_cbg        The I2S callback group pointing to the application's
+ *                       functions to use for initialization and getting and receiving
+ *                       frames.  For TDM the app_data variable within this
+ *                       struct is NOT used.
+ * \param p_dout         The data output port.  MUST be a 1b port
+ * \param p_fsync        The fsync input port. MUST be a 1b port
+ * \param p_bclk         The bit clock input port. MUST be a 1b port
+ * \param bclk           A clock that will get configured for use with
+ *                       the bit clock
+ * \param tx_offset      The number of bclks from FSYNC transition to the MSB
+ *                       of Slot 0
+ * \param app_data       A pointer to application specific data supplied
+ *                       by the application. May be used for context
+ *                       data specific to each I2S task instance.
+ */
+void tdm_slave_tx_16_init(
+        const i2s_tdm_ctx_t *ctx,
+        const i2s_callback_group_t *const i2s_cbg,
+        const port_t p_dout,
+        const port_t p_fsync,
+        const port_t p_bclk,
+        const xclock_t bclk,
+        const uint32_t tx_offset,
+        void *app_data);
+
+/**
+ * I2S TDM TX 16 ch slave task
+ *
+ * This task performs I2S TDM slave on the provided context which was
+ * initialized with tdm_slave_tx_16_init(). It will perform
+ * callbacks over the i2s_callback_group_t callback group to get
+ * data from the application using this component.
+ * 
+ * This thread outputs assumes 1 output port, 32b word length,
+ * 32b channel length, and 16 channels per frame.
+ *
+ * The component performs I2S TDM slave so will expect the fsync and
+ * bit clock to be driven externally.
+ *
+ * \param ctx             A pointer to the I2S TDM context to use.
+ */
+void i2s_tdm_slave_tx_16_thread(
+        const i2s_tdm_ctx_t *ctx);
+
+/**@}*/ // END: addtogroup hil_i2s_tdm_slave_tx16
+
+/**
+ * \addtogroup hil_i2s_tdm_slave hil_i2s_tdm_slave
+ *
+ * The public API for using the HIL I2S TDM generic slave.
+ * @{
+ */
+
+/**
+ * I2S generic TDM slave context initialization
+ *
+ * This prepares a context for I2S TDM slave on the provided pins.
+ * 
+ * The resulting context can be used with an I2S thread call.
+ *
+ * \param ctx            A pointer to the I2S TDM context to use.
+ * \param i2s_cbg        The I2S callback group pointing to the application's
+ *                       functions to use for initialization and getting and receiving
+ *                       frames.  For TDM the app_data variable within this
+ *                       struct is NOT used.
+ * \param p_dout         An array of data output ports.  MUST be 1b ports
+ * \param num_out        The number of output data ports
+ * \param p_din          An array of data input ports.  MUST be 1b ports
+ * \param num_in         The number of input data ports
+ * \param p_fsync        The fsync input port. MUST be a 1b port
+ * \param p_bclk         The bit clock input port. MUST be a 1b port
+ * \param bclk           A clock that will get configured for use with
+ *                       the bit clock
+ * \param tx_offset      The number of bclks from FSYNC transition to the MSB
+ *                       of Slot 0 
+ * \param fsync_len      The length of the FSYNC in BCLKS
+ * \param word_len       The number of bits in each sample frame slot.
+ *                       MUST be 32.
+ * \param ch_len         The number of bits in each channel. MUST be less than
+ *                       word_len
+ * \param ch_per_frame   The number of channels per frame
+ * \param slave_bclk_pol The polarity of bclk
+ * \param app_data       A pointer to application specific data supplied
+ *                       by the application. May be used for context
+ *                       data specific to each I2S task instance.
+ */
+void i2s_tdm_slave_init(
+        const i2s_tdm_ctx_t *ctx,
+        const i2s_callback_group_t *const i2s_cbg,
+        const port_t p_dout[],
+        const size_t num_out,
+        const port_t p_din[],
+        const size_t num_in,
+        const port_t p_fsync,
+        const port_t p_bclk,
+        const xclock_t bclk,
+        const uint32_t tx_offset,
+        const uint32_t fsync_len,
+        const uint32_t word_len,
+        const uint32_t ch_len,
+        const uint32_t ch_per_frame,
+        i2s_slave_bclk_polarity_t slave_bclk_pol,
+        void *app_data);
+
+/**
+ * I2S generic TDM slave task
+ *
+ * This task performs I2S TDM slave on the provided context. It will perform
+ * callbacks over the i2s_callback_group_t callback group to get/receive
+ * data from the application using this component.
+ *
+ * The component performs I2S TDM slave so will expect the fsync and
+ * bit clock to be driven externally.
+ *
+ * \param ctx             A pointer to the I2S TDM context to use.
+ */
+void i2s_slave_tdm_thread(
+        const i2s_tdm_ctx_t *ctx);
+
+/**@}*/ // END: addtogroup hil_i2s_tdm_slave
---
+++ modules/i2s/src/i2s_tdm_slave.c
@@ -0,0 +1,183 @@
+// Copyright 2023 XMOS LIMITED.
+// This Software is subject to the terms of the XMOS Public Licence: Version 1.
+#include <string.h>
+#include <xclib.h>
+#include <xcore/port.h>
+#include <xcore/clock.h>
+#include <xcore/assert.h>
+
+#include "i2s.h"
+#include "i2s_tdm.h"
+
+void i2s_tdm_slave_init(
+        const i2s_tdm_ctx_t *ctx,
+        const i2s_callback_group_t *const i2s_cbg,
+        const port_t p_dout[],
+        const size_t num_out,
+        const port_t p_din[],
+        const size_t num_in,
+        const port_t p_fsync,
+        const port_t p_bclk,
+        const xclock_t bclk,
+        const uint32_t tx_offset,
+        const uint32_t fsync_len,
+        const uint32_t word_len,
+        const uint32_t ch_len,
+        const uint32_t ch_per_frame,
+        i2s_slave_bclk_polarity_t slave_bclk_pol,
+        void *app_data);
+{
+    memset(&ctx, 0, sizeof(i2s_tdm_ctx_t));
+    ctx->i2s_cbg = i2s_cbg;
+
+    xassert(num_out <= I2S_TDM_MAX_POUT_CNT);
+    memcpy(ctx->p_dout, p_dout, sizeof(port_t) * num_out);
+    xassert(num_in <= I2S_TDM_MAX_PIN_CNT);
+    memcpy(ctx->p_din, p_din, sizeof(port_t) * num_in);
+
+    ctx->p_fsync = p_fsync;
+    ctx->p_bclk = p_bclk;
+    ctx->bclk = bclk;
+    ctx->tx_offset = tx_offset;
+    ctx->fsync_len = 1; /* Does not matter for slave as we only care about rising or falling edge */
+    ctx->word_len = word_len;
+    ctx->ch_len = ch_len;
+    ctx->ch_per_frame = ch_per_frame;
+    ctx->slave_bclk_polarity = I2S_SLAVE_SAMPLE_ON_BCLK_RISING;
+    ctx->app_data = app_data;
+}
+
+void i2s_tdm_slave_tx_16_init(
+        const i2s_tdm_ctx_t *ctx,
+        const i2s_callback_group_t *const i2s_cbg,
+        const port_t p_fsync,
+        const port_t p_dout,
+        const port_t p_bclk,
+        const xclock_t bclk,
+        const uint32_t tx_offset,
+        i2s_slave_bclk_polarity_t slave_bclk_polarity,
+        void *app_data))
+{
+    memset(&ctx, 0, sizeof(i2s_tdm_ctx_t));
+    port_t pdout = p_dout;
+
+    i2s_tdm_slave_init(
+        ctx,
+        i2s_cbg,
+        &pdout,
+        1,
+        NULL,
+        0,
+        p_fsync,
+        p_bclk,
+        bclk,
+        tx_offset,
+        1,  /* fsync_len: Does not matter for slave as we only care about edge */
+        32, /* word len */
+        32, /* ch len */
+        16  /* ch per frame */
+        slave_bclk_polarity,
+        app_data);
+}
+
+static void i2s_tdm_slave_init_resources(
+    const i2s_tdm_ctx_t *ctx)
+{
+    port_enable(ctx->p_bclk);
+    clock_enable(ctx->bclk);
+    clock_set_source_port(ctx->bclk, ctx->p_blck);
+
+    for(int i=0; i<ctx->num_out; i++) {
+        port_start_buffered(ctx->p_dout[i], 32);
+        port_set_clock(ctx->p_dout[i], ctx->bclk);
+    }
+    for(int i=0; i<ctx->num_in; i++) {
+        port_start_buffered(ctx->p_din[i], 32);
+        port_set_clock(ctx->p_din[i], ctx->bclk);
+    }
+    port_start_buffered(ctx->p_fsync, 32);
+    port_set_clock(ctx->p_fsync, ctx->bclk);
+    
+    if (ctx->slave_bclk_polarity == I2S_SLAVE_SAMPLE_ON_BCLK_FALLING) {
+        set_port_inv(ctx->p_bclk);
+    } else {
+        set_port_no_inv(ctx->p_bclk);
+    }
+}
+
+static void i2s_tdm_slave_deinit_resources(
+    const i2s_tdm_ctx_t *ctx)
+{
+    clock_disable(ctx->bclk);
+    port_disable(ctx->p_bclk);
+
+    for(int i=0; i<ctx->num_out; i++) {
+        port_disable(ctx->p_dout[i]);
+    }
+    for(int i=0; i<ctx->num_in; i++) {
+        port_disable(ctx->p_din[i]);
+    }
+}
+
+void i2s_tdm_slave_tx_16(
+        const i2s_tdm_ctx_t *ctx)
+{
+    uint32_t out_samps[I2S_TDM_MAX_CH_PER_FRAME];
+    uint32_t fsync_valid;
+
+    if (ctx->i2s_cbg->init != NULL) {
+        ctx->i2s_cbg->init((void*)ctx, NULL);
+    }
+    xassert(ctx->num_out == 1);
+
+    i2s_tdm_slave_init_resources(ctx);
+
+    uint32_t port_frame_time = (ctx->ch_per_frame * ctx->word_len);
+
+    /* Determine bclks for tx offset setting */
+    uint32_t tx_offset_word_clks = ctx->tx_offset / 32;
+    uint32_t tx_offset_rem_clks = ctx->tx_offset % 32;
+
+    /* Wait for first fsync rising edge to occur */
+    port_set_trigger_in_equal(ctx->p_fsync, 0);
+    (void) port_in(ctx->p_fsync);
+    port_set_trigger_in_equal(ctx->p_fsync, 1);
+    (void) port_in(ctx->p_fsync);
+    port_timestamp_t fsync_edge_time = port_get_trigger_time(ctx->p_fsync);
+
+    port_set_trigger_time(ctx->p_dout[0], port_frame_time + fsync_edge_time);
+
+    while(1) {
+        /* Assume at this point in time we are always at fsync edge */
+        /* TODO how would we best check that we have stayed in sync also what if we did get out of sync? do we just implicitly exit or do we add another callback to alert app */
+        for (int i=0; i<tx_offset_word_clks; i++) {
+            (void) port_in(ctx->p_dout[0]);
+        }
+        if (tx_offset_rem_clks > 0) {
+            port_set_shift_count(ctx->p_dout[0], tx_offset_rem_clks);
+            (void) port_in(ctx->p_dout[0]);
+            port_set_shift_count(ctx->p_dout[0], ctx->word_len);
+        }
+
+        /* Get frame data and tx */
+        ctx->i2s_cbg->send((void*)ctx, ctx->ch_per_frame, out_samps);
+        
+        for (int i=0; i<ctx->ch_per_frame; i++) {
+            port_out(ctx->p_dout[0], bitrev(out_samps[i]));
+        }
+
+        /* Check for exit condition */
+        if (ctx->i2s_cbg->restart_check != NULL) {
+            if (ctx->i2s_cbg->restart_check(ctx) == I2S_SHUTDOWN) {
+                i2s_tdm_slave_deinit_resources(ctx);
+                return;
+            }
+        }
+    }
+}
+
+void i2s_slave_tdm_thread(
+        const i2s_tdm_ctx_t *ctx)
+{
+    xassert(0); /* Not yet implemented */
+}

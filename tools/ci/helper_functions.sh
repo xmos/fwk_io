72cad6458d02d112b3a4d7e0d9104c5ce2f07c2c
Shuchita Khare <shuchitak@xmos.com>
2024-02-22 10:31:25 +0000

    fix calling of .sh script

--- tools/ci/helper_functions.sh
+++ tools/ci/helper_functions.sh
@@ -5,11 +5,11 @@
 
 # A small utility to run a command and only print logs if the command fails.
 # On success, all logs are hidden.
-function log_errors {
+log_errors {
     if log=$("$@" 2>&1); then
         echo "'$@' completed successfully!"
     else
         echo "$log"
         return 1
-    fi    
+    fi
 }
